34c563d453503892e1cb6827e2461f229a1e2ce7
minseopKim <22200078@handong.ac.kr>
2025-02-21 16:17:39 +0900

    basic_component create

--- src/index.js
+++ src/index.js
@@ -6,29 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
 import Clock from "./chapter_04/Clock";
+import CommentList from "./chapter_05/CommentList";
 
 const container = document.getElementById("root");
 
 if (container) {
   const root = ReactDOM.createRoot(container);
-
-  const tick = () => {
-    root.render(
-      <React.StrictMode>
-        <Clock />
-      </React.StrictMode>
-    );
-  };
-
-  setInterval(tick, 1000);
+  root.render(
+    <React.StrictMode>
+      <CommentList />
+    </React.StrictMode>
+  );
 } else {
   console.error("⚠️ 'root' 엘리먼트를 찾을 수 없습니다.");
 }
-
-/**
- * 리액트 Elements는 자바스크립트 객체 형태로 존재한다.
- * 그리고 리액트 Elements는 immutable하고 불변성을 가지고 있다.
- * 이것은 Elements 생성 후에는 children이나 attributes를 바꿀 수 없습니다.
- */
 
 reportWebVitals();
