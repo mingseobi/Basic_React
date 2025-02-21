2a1c86ddb34cf3805ae7f84c1e1fe667fe0a3f5b
minseopKim <22200078@handong.ac.kr>
2025-02-21 15:49:05 +0900

    create clock system

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/Clock";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = ReactDOM.createRoot(container);
+
+  const tick = () => {
+    root.render(
+      <React.StrictMode>
+        <Clock />
+      </React.StrictMode>
+    );
+  };
+
+  setInterval(tick, 1000);
+} else {
+  console.error("⚠️ 'root' 엘리먼트를 찾을 수 없습니다.");
+}
 
 /**
  * 리액트 Elements는 자바스크립트 객체 형태로 존재한다.
@@ -20,7 +31,4 @@ root.render(
  * 이것은 Elements 생성 후에는 children이나 attributes를 바꿀 수 없습니다.
  */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
