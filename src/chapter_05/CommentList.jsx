716f4feef0a8986cfa8bf36562898ba4bcbc44a2
minseopKim <22200078@handong.ac.kr>
2025-02-25 09:21:09 +0900

    basic component css update

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from "./Comment";
 function CommentList(props) {
   return (
     <div>
-      <Comment />
+      <Comment name={"ㅎㅇㅎㅇ"} comment={"안녕하세요, 소플입니다."} />
+      <Comment name={"ㅋㅋ"} comment={"ㅎㅇ, 소플입니다."} />
     </div>
   );
 }
